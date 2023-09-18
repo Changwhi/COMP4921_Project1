8874af7f4289813d4e6592d1d895dde76e1d37a7
Changwhi <whinada@gmail.com>
2023-09-18 08:31:04 -0700

    refactor: Rearrange code structure for improved organization

--- routes/router.js
+++ routes/router.js
@@ -9,6 +9,34 @@ const expireTime = 60 * 60 * 1000;  // session expire time, persist for 1 hour.
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
+//Cloudinary
+const database = include('databaseConnectionMongoDB');
+var ObjectId = require('mongodb').ObjectId;
+
+
+const cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_CLOUD_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
+
+const multer  = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+const mongodb_database = process.env.REMOTE_MONGODB_DATABASE;
+const userCollection = database.db(mongodb_database).collection('users');
+const petCollection = database.db(mongodb_database).collection('pets');
+const userPicCollection = database.db(mongodb_database).collection('userPicture');
+
+const Joi = require("joi");
+const mongoSanitize = require('express-mongo-sanitize');
+
+router.use(mongoSanitize(
+    {replaceWith: '%'}
+));
+
 //router.use(mongoSanitize(
 //    {replaceWith: '%'}
 //));
@@ -39,7 +67,7 @@ router.use(session({
 
 //* User is brought to index page to login or sign up */
 router.get('/', async (req, res) => {
-    res.render('index');
+    res.render('images');
 });
 //* Successful or unsuccessful login*/
 router.post('/loggingin', (req,res) => {
@@ -81,6 +109,48 @@ router.get('/tempLoggedin', (req,res) => {
     You are logged in!
     `;
     res.send(html);
+});
+
+
+router.get('/showPics', async (req, res) => {
+	console.log("picture page");
+	try {
+		let user_id = req.query.id;
+		console.log("userId: "+user_id);
+
+		// Joi validate
+		const schema = Joi.object(
+			{
+				user_id: Joi.string().alphanum().min(24).max(24).required()
+			});
+		
+		const validationResult = schema.validate({user_id});
+		if (validationResult.error != null) {
+			console.log(validationResult.error);
+
+			res.render('error', {message: 'Invalid user_id'});
+			return;
+		}				
+		const pics= await userPicCollection.find({"user_id": new ObjectId(user_id)}).toArray();
+
+		if (pics === null) {
+			res.render('error', {message: 'Error connecting to MongoDB'});
+			console.log("Error connecting to userModel");
+		}
+		else {
+			pics.map((item) => {
+				item.pic_id = item._id;
+				return item;
+			});			
+			console.log(pics);
+			res.render('images', {allPics: pics, user_id: user_id});
+		}
+	}
+	catch(ex) {
+		res.render('error', {message: 'Error connecting to MongoDB'});
+		console.log("Error connecting to MongoDB");
+		console.log(ex);
+	}
 });
 
 // router.get('/login', (req,res) => {
