ab2d5e7f3e558e3a2259774c3b4a457e537fc6a7
Changwhi <whinada@gmail.com>
2023-09-26 20:57:35 -0700

    Feat: Implements session validation

--- routes/router.js
+++ routes/router.js
@@ -72,54 +72,6 @@ router.use(
 );
 //testing
 
-router.get("/pic", async (req, res) => {
-  res.send(
-    '<form action="picUpload" method="post" enctype="multipart/form-data">' +
-      '<p>Public ID: <input type="text" name="title"/></p>' +
-      '<p>Image: <input type="file" name="image"/></p>' +
-      '<p><input type="submit" value="Upload"/></p>' +
-      "</form>"
-  );
-});
-
-router.post("/picUpload", upload.single("image"), function (req, res, next) {
-  let buf64 = req.file.buffer.toString("base64");
-  stream = cloudinary.uploader.upload(
-    "data:image/png;base64," + buf64,
-    function (result) {
-      //_stream
-      console.log(result);
-      res.send(
-        'Done:<br/> <img src="' +
-          result.url +
-          '"/><br/>' +
-          cloudinary.image(result.public_id, {
-            format: "png",
-            width: 100,
-            height: 130,
-            crop: "fit",
-          })
-      );
-    },
-    { public_id: req.body.title }
-  );
-  console.log(req.body);
-  console.log(req.file);
-});
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-//ttesting
-
-function isValidSession(req, res, next) {
-  if (req.session.authenticated) {
-      next();
-  } else {
-      res.render("error", {message: "Not Authenticated"});
-  }
-}
-
 //* User is brought to index page to login or sign up */
 router.get("/", async (req, res) => {
   console.log("index page hit");
@@ -149,17 +101,139 @@ router.get("/", async (req, res) => {
   }
 });
 
+
+function isValidSession(req, res, next) {
+  if (req.session.authenticated) {
+    next();
+  } else {
+    res.render("error", { message: "Not Authenticated" });
+  }
+}
+
+
 router.get("/login", async (req, res) => {
-	console.log("index page hit");
+  console.log("index page hit");
 
-		res.render("login");
+  res.render("login");
 
-  });
+});
 router.get("/signup", async (req, res) => {
-	console.log("index page hit");
-	res.render("signup");
+  console.log("index page hit");
+  res.render("signup");
 
-  });
+});
+
+router.post("/loggingin", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  var users = await db_users.getUsers();
+  var user;
+  for (i = 0; i < users.length; i++) {
+    console.info(users[i].email);
+    if (users[i].email == email) {
+      user = users[i];
+    }
+  }
+  for (i = 0; i < users.length; i++) {
+    const isValidPassword = bcrypt.compareSync(password, user.hashed_password)
+    if (user.email == email) {
+      if (isValidPassword) {
+        req.session.userID = user.user_id
+        req.session.authenticated = true;
+        req.session.email = email;
+        req.session.cookie.maxAge = expireTime;
+        res.redirect('/link');
+        return
+      }
+      else if (!isValidPassword) {
+        req.session.authenticated = false;
+        res.redirect('/login');
+        return;
+      }
+    }
+  }
+  //User & PW combo not found.
+  res.render("login");
+});
+//** CREATING THE USER SECTION */
+//** Render tempUserSignup which is /createUser originally, renamed for temp use. */
+//* Middleware for hashing password and pushing into mySQL DB*/
+router.post("/submitUser", async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  var name = req.body.name;
+  var hashedPassword = bcrypt.hashSync(password, saltRounds);
+  var success = await db_users.createUser({ email: email, hashedPassword: hashedPassword, name: name });
+  if (success) {
+    res.redirect('/login')
+  } else if (!success) {
+    res.render('error', { message: `Failed to create the user ${email}, ${name}`, title: "User creation failed" })
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Login Page" });
+});
+
+function isValidSession(req) {
+  if (req.session.authenticated) {
+    return true;
+  }
+  return false;
+}
+
+function sessionValidation(req, res, next) {
+  if (!isValidSession(req)) {
+    req.session.destroy();
+    res.redirect('/login');
+    return;
+  }
+  else {
+    next();
+  }
+}
+
+//******************************************** After logged in **************************
+app.use('/loggedin', sessionValidation);
+
+router.get("/pic", async (req, res) => {
+  res.send(
+    '<form action="picUpload" method="post" enctype="multipart/form-data">' +
+    '<p>Public ID: <input type="text" name="title"/></p>' +
+    '<p>Image: <input type="file" name="image"/></p>' +
+    '<p><input type="submit" value="Upload"/></p>' +
+    "</form>"
+  );
+});
+
+router.post("/picUpload", upload.single("image"), function(req, res, next) {
+  let buf64 = req.file.buffer.toString("base64");
+  stream = cloudinary.uploader.upload(
+    "data:image/png;base64," + buf64,
+    function(result) {
+      //_stream
+      console.log(result);
+      res.send(
+        'Done:<br/> <img src="' +
+        result.url +
+        '"/><br/>' +
+        cloudinary.image(result.public_id, {
+          format: "png",
+          width: 100,
+          height: 130,
+          crop: "fit",
+        })
+      );
+    },
+    { public_id: req.body.title }
+  );
+  console.log(req.body);
+  console.log(req.file);
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 router.get("/link", isValidSession, async (req, res) => {
   console.log("index page hit");
@@ -188,67 +262,6 @@ router.get("/link", isValidSession, async (req, res) => {
   }
 });
 
-//* Login check */
-router.post("/loggingin", async (req, res) => {
-  var email = req.body.email;
-  var password = req.body.password;
-  var users = await db_users.getUsers();
-  var user;
-  for (i = 0; i < users.length; i++) {
-    console.info(users[i].email);
-    if (users[i].email == email) {
-        user = users[i];
-    }
-  }
-  for (i = 0; i < users.length; i++) {
-      const isValidPassword = bcrypt.compareSync(password, user.hashed_password)
-      if (user.email == email) {
-          if (isValidPassword){
-              req.session.userID = user.user_id
-              req.session.authenticated = true;
-              req.session.email = email;
-              req.session.cookie.maxAge = expireTime;
-              res.redirect('/link');
-              return
-          }
-          else if (!isValidPassword){
-              req.session.authenticated = false;
-              res.redirect('/login');
-              return;
-          }
-      }
-  }
-  //User & PW combo not found.
-  res.render("login");
-});
-//** CREATING THE USER SECTION */
-//** Render tempUserSignup which is /createUser originally, renamed for temp use. */
-router.get("/createUser", (req, res) => {
-  //TODO render awesome custom & stylized Sign Up page by Changwhi
-  res.render("tempUserSignup", { title: "Sign up Page" });
-});
-
-//* Middleware for hashing password and pushing into mySQL DB*/
-router.post("/submitUser", async (req, res) => {
-  var email = req.body.email;
-  var password = req.body.password;
-  var name = req.body.name;
-  var hashedPassword = bcrypt.hashSync(password, saltRounds);
-  var success = await db_users.createUser({email: email, hashedPassword: hashedPassword, name: name});
-  if (success) {
-    res.redirect('/login')
-  } else if (!success) {
-    res.render('error', {message: `Failed to create the user ${email}, ${name}`, title: "User creation failed"})
-  }
-});
-
-
-router.get("/tempLoggedin", (req, res) => {
-  var html = `
-    You are logged in!
-    `;
-  res.send(html);
-});
 
 router.get("/showPics", async (req, res) => {
   console.log("picture page");
@@ -282,7 +295,7 @@ router.get("/showPics", async (req, res) => {
         return item;
       });
       console.log(pics);
-      res.render("images", {allPics: pics, user_id: user_id});
+      res.render("images", { allPics: pics, user_id: user_id });
     }
   } catch (ex) {
     res.render("error", { message: "Error connecting to MongoDB" });
@@ -322,22 +335,22 @@ router.post("/addpic", async (req, res) => {
       comment: req.body.comment,
     });
 
-	res.redirect(`/showPics?id=${user_id}`);
-} catch (ex) {
+    res.redirect(`/showPics?id=${user_id}`);
+  } catch (ex) {
     res.render("error", { message: "Error connecting to MySQL" });
     console.log("Error connecting to MySQL");
     console.log(ex);
   }
 });
 
-router.post("/setUserPic", upload.single("image"), function (req, res, next) {
+router.post("/setUserPic", upload.single("image"), function(req, res, next) {
   let image_uuid = uuid();
   let pic_id = req.body.pic_id;
   let user_id = req.body.user_id;
   let buf64 = req.file.buffer.toString("base64");
   stream = cloudinary.uploader.upload(
     "data:image/octet-stream;base64," + buf64,
-    async function (result) {
+    async function(result) {
       try {
         console.log(result);
 
@@ -383,60 +396,56 @@ router.post("/setUserPic", upload.single("image"), function (req, res, next) {
 });
 
 router.get('/deletePics', async (req, res) => {
-	try {
-		console.log("delete pet image");
+  try {
+    console.log("delete pet image");
 
-		let pet_id = req.query.id;
-		let user_id = req.query.user;
-		let is_user_pic = req.query.pic;
-		let pic_id = req.query.id;
+    let pet_id = req.query.id;
+    let user_id = req.query.user;
+    let is_user_pic = req.query.pic;
+    let pic_id = req.query.id;
 
-		const schema = Joi.object(
-			{
-				user_id: Joi.string().alphanum().min(24).max(24).required(),
-				pet_id: Joi.string().alphanum().min(24).max(24).required(),
-			});
+    const schema = Joi.object(
+      {
+        user_id: Joi.string().alphanum().min(24).max(24).required(),
+        pet_id: Joi.string().alphanum().min(24).max(24).required(),
+      });
 
-		const validationResult = schema.validate({user_id, pet_id});
+    const validationResult = schema.validate({ user_id, pet_id });
 
-		if (validationResult.error != null) {
-			console.log(validationResult.error);
+    if (validationResult.error != null) {
+      console.log(validationResult.error);
 
-			res.render('error', {message: 'Invalid user_id or pet_id'});
-			return;
-		}
+      res.render('error', { message: 'Invalid user_id or pet_id' });
+      return;
+    }
 
-		if (is_user_pic == 'true') {
-			console.log("pic_id: "+pet_id);
-			const success = await userPicCollection.updateOne({"_id": new ObjectId(pic_id)},
-				{$set: {image_id: undefined}},
-				{}
-			);
+    if (is_user_pic == 'true') {
+      console.log("pic_id: " + pet_id);
+      const success = await userPicCollection.updateOne({ "_id": new ObjectId(pic_id) },
+        { $set: { image_id: undefined } },
+        {}
+      );
 
-			console.log("delete User Image: ");
-			console.log(success);
-			if (!success) {
-				res.render('error', {message: 'Error connecting to MySQL'});
-				return;
-			}
-		res.redirect(`/showPics`);
+      console.log("delete User Image: ");
+      console.log(success);
+      if (!success) {
+        res.render('error', { message: 'Error connecting to MySQL' });
+        return;
+      }
+      res.redirect(`/showPics`);
 
-		}
-	}
-	catch(ex) {
-		res.render('error', {message: 'Error connecting to MySQL'});
-		console.log("Error connecting to MySQL");
-		console.log(ex);
-	}
+    }
+  }
+  catch (ex) {
+    res.render('error', { message: 'Error connecting to MySQL' });
+    console.log("Error connecting to MySQL");
+    console.log(ex);
+  }
 });
 
-
-router.get("/login", (req, res) => {
-  res.render("login", { title: "Login Page" });
-});
 
 router.get('*', (req, res) => {
-	res.status(404).render('error', {message: "404 No such page found."})
+  res.status(404).render('error', { message: "404 No such page found." })
 })
 
 module.exports = router;
