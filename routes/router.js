e5ee2f7f559a24398c53ee9d977de406faf2c385
Felix L. <flieu@my.bcit.ca>
2023-10-04 21:24:08 -0700

    fix: added other patterns inside error message

--- routes/router.js
+++ routes/router.js
@@ -124,14 +124,18 @@ router.post("/loggingin", async (req, res) => {
     }
   }
   const passwordSchema = Joi.object({
-    password: Joi.string().alphanum().pattern(/(?=.*[a-z])/).pattern(/(?=.*[A-Z])/).min(12).max(50).required(),
+    password: Joi.string().alphanum().pattern(/(?=.*[a-z])/).pattern(/(?=.*[A-Z])/).pattern(/[!@#$%^&*]/).pattern(/[0-9]/).min(12).max(50).required(),
   });
   const validationResult = passwordSchema.validate({ password });
   let errorMsg = validationResult.error.details[0].message
   if (errorMsg.includes("(?=.*[a-z])")) {
     errorMsg = "Password must have at least 1 lowercase."
   } else if (errorMsg.includes("(?=.*[A-Z])")) {
-    errorMsg = "Password must haveat least 1 uppercase."
+    errorMsg = "Password must have at least 1 uppercase."
+  } else if (errorMsg.includes("[!@#$%^&*]")) {
+    errorMsg = "Password requires 1 special character."
+  } else if (errorMsg.includes("[0-9]")) {
+    errorMsg = "Password needs to have 1 number."
   }
   if (validationResult.error != null) {
     res.render("login", { message: errorMsg, isLoggedIn: false});
